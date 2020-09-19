e63d86893c980b7072d5e1a835e41b4330d0277f
SHAIKH MUDASSIR <skmudassirahmed@gmail.com>
2020-09-19 16:52:17 +0000

    Update links.js

--- sample/links.js
+++ sample/links.js
@@ -1,7 +1,10 @@
+var title = "Sample";
 var links = [
   "Task 1",
   "Task 2"
 ];
+
+document.getElementsByTagName("title").innerText = "CodeBase - "+ title
 
 hold = document.getElementById("links").innerHTML;
 holdres = document.getElementById("links-res").innerHTML;
@@ -9,7 +12,7 @@ for(var i=0; i < links.length; i++)
 {
   document.getElementById("links").innerHTML = hold + "<a href='"+ i +"'><button class='btn btn-light'>"+ links[i] +"</button></a>";
   hold = document.getElementById("links").innerHTML;
-  document.getElementById("links-res").innerHTML = holdres + "<a href='"+ i +"'><button class='btn btn-light'>"+ links[i] +"</button></a>"links[i];
+  document.getElementById("links-res").innerHTML = holdres + "<a href='"+ i +"'><button class='btn btn-light'>"+ links[i] +"</button></a>";
   holdres = document.getElementById("links-res").innerHTML;
 }
 
